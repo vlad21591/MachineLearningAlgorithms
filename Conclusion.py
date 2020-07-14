5f358825a261ae90e7dd308c083a3c348f4b9902
vlad21591 <59918077+vlad21591@users.noreply.github.com>
2020-07-14 20:53:16 +0300

    Update Conclusion.py

--- Conclusion.py
+++ Conclusion.py
@@ -31,3 +31,12 @@ for name, mdl in models.items():
 # We can see that the RF_model is the best fit
 print("------------------------------ Best Model is RF - Random Forest -------------------------------")
 evaluate_model('Random Forest', models['RF'], test_features,test_labels)
+
+# Output:
+# LR -- Accuracy: 0.77, Precision: 0.707, Recall:0.631, Latency: 0.0ms
+# SVM -- Accuracy: 0.747, Precision: 0.672, Recall:0.6, Latency: 0.0ms
+# MLP -- Accuracy: 0.747, Precision: 0.667, Recall:0.615, Latency: 0.0ms
+# RF -- Accuracy: 0.82, Precision: 0.824, Recall:0.646, Latency: 0.0ms
+# GB -- Accuracy: 0.798, Precision: 0.784, Recall:0.615, Latency: 0.007988929748535156ms
+# ------------------------------ Best Model is RF - Random Forest -------------------------------
+# Random Forest -- Accuracy: 0.81, Precision: 0.875, Recall:0.645, Latency: 0.0ms
